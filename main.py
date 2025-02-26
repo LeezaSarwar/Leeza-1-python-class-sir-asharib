a933675f830b2e33c282d25cb9867202dc2698f1
Leeza Sarwar <leezasarwar0@gmail.com>
2025-02-26 17:23:00 +0500

    main.py
    main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+name:str= 'GIAIC (Governor Iniatiative For Ai And Computing)'
+print ("GIAIC stand for : ",name )
+print("Hello World From Leeza")
+
+
+def giaic():
+    print("hello world")
+
+    
+# string
+var_str ="Hello world"
+print(var_str)
+
+
+if True:
+     print("monday 2pm to 5pm ")
+id_card:str ="""""Name:LEEZA
+Father name: SARWAR
+age : 17"""
+print(id_card)
+
+
+float
+flaot_num: float=1.5
+print(type(flaot_num))
+
+# boolean
+bool
+is_true = True
+is_false = False
+print(type(is_true))
+
+
+tuple
+tuple_data:tuple = ("apple","cherry")
+print(type(tuple_data))
+print(tuple_data[0])
+
+list
+fruit:list[str]=["apple","mango","banana" ,"cherry"]
+print(type(fruit[0])) 
+print(fruit[-1])
+
+dict [str]=("leeza sarwar")
+name :dict
+print(type(name[0]))
+print(name[-1])
