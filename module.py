171e03f4f1e47dc937dbb9b91b34398cb558b9b0
Leeza Sarwar <leezasarwar0@gmail.com>
2025-02-26 17:34:33 +0500

    Create module.py

---
+++ module.py
@@ -0,0 +1,19 @@
+String (str) ex:  Ordered collection of characters Can be enclosed in single quotes or double quotes Example: "hello", 'hello', "hello world"
+
+Integer number (int) ex:  Whole numbers, either positive, negative, or zero Example: 1, 2, 3, -1, -2, - 
+
+Float  Decimal numbers, either positive, negative, or zero Example: 3.14, -0.5, 0.0
+
+Boolean (bool)  Logical values that can be either True or False  Example: True, False
+
+list (list) Ordered collection of items that can be of any data type Example: [1, 2, 3], ["a", "b", "c"], [1, "a", 2.5]
+
+Tuple (tuple) Ordered, immutable collection of items that can be of any data type  Example: (1, 2, 3), ("a", "b", "c"), (1, "a", 2.5)
+
+None type (none) ex: special value that represent the adsence of a value or a null object reference ,none
+
+Dictionary (dict)   ex: name; leeza age;17
+
+Frozen (frozenset) ex: {1,2,3} {a,b,c}
+
+set (set){1,2,3} {a,b,c}
